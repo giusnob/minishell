cb2143247eff41134a50ed4c678275bb969ceca3
Giulia Fanelli <gifanell@student.42firenze.it>
2026-01-06 21:19:14 +0100

    fix: improve export variable handling by adding sorting functions

--- src/builtin_export.c
+++ src/builtin_export.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   builtin_export.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gifanell <giuliafanelli111@gmail.com>      +#+  +:+       +#+        */
+/*   By: gifanell <gifanell@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/26 17:00:19 by ginobile          #+#    #+#             */
-/*   Updated: 2026/01/05 05:22:03 by gifanell         ###   ########.fr       */
+/*   Updated: 2026/01/06 21:11:43 by gifanell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ static int	process_export_arg(char *arg, t_data *data);
 static void	handle_export_with_value(char *key, char *value, t_data *data);
 static void	print_export_marks(t_data *data);
 static void	print_export_vars(t_data *data);
+static void	sort_export_array(char **arr);
+static char	**copy_array(char **arr);
 
 /* Stampa una singola variabile export */
 static void	print_single_export(char *env_var)
@@ -43,21 +45,77 @@ static void	print_single_export(char *env_var)
 	}
 	write(STDOUT_FILENO, "\n", 1);
 }
+
+static char	**copy_array(char **arr)
+{
+	int		len;
+	char	**copy;
+	int		i;
+
+	len = 0;
+	while (arr[len])
+		len++;
+	copy = malloc(sizeof(char *) * (len + 1));
+	if (!copy)
+		return (NULL);
+	i = 0;
+	while (i < len)
+	{
+		copy[i] = arr[i];  // Copia solo i puntatori, non le stringhe
+		i++;
+	}
+	copy[i] = NULL;
+	return (copy);
+}
 /*Stampa export marks (variabili ambientali senza valore)*/
+static void	sort_export_array(char **arr)
+{
+	int		i;
+	int		j;
+	int		len;
+	char	*temp;
+
+	len = 0;
+	while (arr[len])
+		len++;
+	i = 0;
+	while (i < len - 1)
+	{
+		j = 0;
+		while (j < len - i - 1)
+		{
+			if (ft_strcmp(arr[j], arr[j + 1]) > 0)
+			{
+				temp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = temp;
+			}
+			j++;
+		}
+		i++;
+	}
+}
+
 static void	print_export_marks(t_data *data)
 {
-	int i;
+	int		i;
+	char	**sorted;
 
 	if (!data->export_marks)
 		return ;
+	sorted = copy_array(data->export_marks);  // Usa la funzione dell'opzione 2
+	if (!sorted)
+		return ;
+	sort_export_array(sorted);
 	i = 0;
-	while (data->export_marks[i])
+	while (sorted[i])
 	{
 		ft_putstr_fd("declare -x ", STDOUT_FILENO);
-		ft_putstr_fd(data->export_marks[i], STDOUT_FILENO);
+		ft_putstr_fd(sorted[i], STDOUT_FILENO);
 		write(STDOUT_FILENO, "\n", 1);
 		i++;
 	}
+	free(sorted);
 }
 
 /* Stampa tutte le variabili d'ambiente in formato export */
